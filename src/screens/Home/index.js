5b2220a77996de376c13d0e98e5f0c56fad786f7
samad324 <a.s324889@gmail.com>
2019-03-29 15:39:12 +0500

    4-driver-apply-basic-redux-saga-3:redux saga

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,26 +1,17 @@
-import React, {Component} from 'react';
-import {
-  StyleSheet, 
-  Text, 
-  View,
-  Image,
-  Button
-} from 'react-native';
-import Rocket from '../../assets/images/rocket.gif';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import Rocket from "../../assets/images/rocket.gif";
 
 export default class Home extends Component {
   render() {
     return (
-      <View  style={styles.container}>
+      <View style={styles.container}>
         <Image source={Rocket} style={styles.welcomeImage} />
         <Text style={styles.welcomeText}>Hello Comingoo !!</Text>
         <Button
           title="Login"
-          onPress={() =>
-            this.props.navigation.navigate('Login')
-          }
+          onPress={() => this.props.navigation.navigate("Login")}
         />
-        
       </View>
     );
   }
@@ -29,19 +20,16 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-   
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff"
   },
   welcomeImage: {
-    height : 100,
-    width : 100,
+    height: 100,
+    width: 100
   },
 
-  welcomeText : {
-    fontSize : 20
+  welcomeText: {
+    fontSize: 20
   }
-  
 });
-
