90445fdabde85e8cf1755c632ffb9009d8d0777d
samad324 <a.s324889@gmail.com>
2019-03-29 18:13:45 +0500

    18-driver-initial-screen-3:initial screen done

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,35 +1,42 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image, Button } from "react-native";
-import Rocket from "../../assets/images/rocket.gif";
+import { Text, View, Image, ScrollView } from "react-native";
+
+import { images } from "../../utils";
+import styles from "./styles";
 
 export default class Home extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Image source={Rocket} style={styles.welcomeImage} />
-        <Text style={styles.welcomeText}>Hello Comingoo !!</Text>
-        <Button
-          title="Login"
-          onPress={() => this.props.navigation.navigate("Login")}
-        />
-      </View>
+      <ScrollView style={styles.container} contentContainerStyle={{ flex: 1 }}>
+        <View style={styles.topContainer}>
+          <Image
+            source={images.comingoo_logo}
+            style={styles.logoStyles}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.middleContainer}>
+          <View style={styles.middleInnerContainer}>
+            <View style={styles.middleTextContainer}>
+              <Text style={styles.smallTxt}>Vous n'avez pas de comple?</Text>
+              <Text style={styles.mediumTxt}>S'inscrire</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.bottomContainer}>
+          <View style={styles.bottomInnerContainer}>
+            <View style={styles.bottomTextContainer}>
+              <Text style={styles.smallTxt}>Vous avez deja un compte?</Text>
+              <Text style={styles.mediumTxt}>Se connecter</Text>
+            </View>
+          </View>
+          <View style={styles.footerContainer}>
+            <Text style={styles.regularTxt}>
+              Recherchez-vous I'application du passenger?
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff"
-  },
-  welcomeImage: {
-    height: 100,
-    width: 100
-  },
-
-  welcomeText: {
-    fontSize: 20
-  }
-});
