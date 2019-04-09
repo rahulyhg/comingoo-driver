e92842072d6a7fa1e3f94610995be1a830ef0c8d
samad324 <a.s324889@gmail.com>
2019-04-09 08:53:19 -0700

    80:Conflicts solved for iOS

--- ios/Pods/Target Support Files/Pods-ComingooDriverApp-ComingooDriverApp-tvOSTests/Pods-ComingooDriverApp-ComingooDriverApp-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ComingooDriverApp_ComingooDriverApp_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_ComingooDriverApp_ComingooDriverApp_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-ComingooDriverApp-ComingooDriverAppTests/Pods-ComingooDriverApp-ComingooDriverAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ComingooDriverApp_ComingooDriverAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_ComingooDriverApp_ComingooDriverAppTests
-@end
